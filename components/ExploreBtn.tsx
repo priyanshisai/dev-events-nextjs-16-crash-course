105e02c56a2a3a076de6c79d17243c2b587c3480
Priyanshi Sai <priyanshi_sai@Priyanshis-MacBook-Air.local>
2026-02-05 22:52:46 +0530

    Implement PostHog

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Image from "next/image";
+import posthog from "posthog-js";
+
+const ExploreBtn = () =>{
+    const handleClick = () => {
+        posthog.capture('explore_events_clicked', {
+            button_location: 'homepage_hero',
+        });
+    };
+
+    return(
+        <button type="button" id="explore-btn" className="mt-07 mx-auto" onClick={handleClick}>
+            <a href="#events">
+            Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+            </a>
+        </button>
+    )
+}
+export default ExploreBtn;
---
+++ lib/constants.ts
@@ -0,0 +1,47 @@
+export type EventItem = {
+    image: string;
+    title: string;
+    slug: string;
+    location: string;
+    date: string;
+    time: string;
+};
+export const events: EventItem[] = [
+    {image:"/images/event1.png" ,
+        title: "React Summit US 2026",
+        slug: "react-summit-us-2026",
+        location: "San Francisco, CA, USA",
+        date: "07/11/2026",
+        time: "09:00 AM",
+    },
+    {image: "/images/event2.png",
+        title: "KubeCon + CloudNativeCon Europe 2026",
+        slug: "kube-con-cloud-native-con-Europe-2026",
+        location: "Vienna, Austria",
+        date: "",
+        time: "",
+    },
+    {image:"/images/event3.png",
+        title: "AWS re:Invent 2026",
+        slug: "aws-re:invent-2026",
+        location: "Las Vegas, NV, USA ",
+        date: "",
+        time: "",
+    },
+    {image:"/images/event4.png" ,
+        title: "Next.js Conf 2026",
+        slug: "next.js-conf-2026",
+        location: "Los Angeles, CA, USA (Hybrid)",
+        date: "",
+        time: "",
+    },
+    {image: "/images/event5.png",
+        title: "GOOGLE CLOUD NEXT 2026",
+        slug: "google-cloud-next-2026",
+        location: "San Jose, CA, USA",
+        date: "07/04/2026",
+        time: "09:00 AM",
+    },
+
+
+]
